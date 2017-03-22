965c4a8ef44dd4059e49c033087803d6d4462ff9
Brian Hardy <lyricsboy@users.noreply.github.com>
2017-03-22 17:37:02 -0400

    Initial version before pod install

---
+++ PodsTestApp/PodsTestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PodsTestApp
+//
+//  Created by Brian Hardy on 3/22/17.
+//  Copyright © 2017 Test, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
